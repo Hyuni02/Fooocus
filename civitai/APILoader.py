25910a61c2d03083e401451e4fcd210b533e941f
Hyuni02 <jackjsh@naver.com>
2024-11-27 15:15:31 +0900

    경로 수정

--- civitai/APILoader.py
+++ civitai/APILoader.py
@@ -2,4 +2,4 @@ def load_api_key(file_name):
     with open(file_name, 'r') as file:
         return file.read()
 
-api = load_api_key('/content/drive/MyDrive/api-key.txt')
+api = load_api_key('./api-key.txt')
