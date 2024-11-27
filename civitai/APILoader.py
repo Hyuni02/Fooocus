e98d8070fd52b000139adc5e0e2007347b54023a
Hyuni02 <jackjsh@naver.com>
2024-11-27 15:51:51 +0900

    ntrmix.json추가

--- civitai/APILoader.py
+++ civitai/APILoader.py
@@ -3,6 +3,4 @@ def load_api_key(file_name):
     with open(file_name, 'r') as file:
         return file.read()
 
-print(os.path.abspath(__file__))
-print(os.path.exists('/content/Fooocus/civitai/api-key.txt'))
 api = load_api_key('/content/Fooocus/civitai/api-key.txt')
