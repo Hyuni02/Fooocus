73dd8dd627a6352c9050b76a66a083913c69b9fb
Hyuni02 <jackjsh@naver.com>
2024-11-27 14:38:40 +0900

    경로 수정

--- modules/model_loader.py
+++ modules/model_loader.py
@@ -1,7 +1,11 @@
 import os
+import sys
 from urllib.parse import urlparse
 from typing import Optional
 
+# 프로젝트의 루트 경로를 추가
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_path)
 
 def load_file_from_url(
         url: str,
@@ -25,9 +29,9 @@ def load_file_from_url(
         print(f'Downloading: "{url}" to {cached_file}\n')
         from torch.hub import download_url_to_file
         if 'civitai' in url:
-            import civitai
+            from civitai.CivitaiDownloader import download_model
             model_id = url.split('/')[-1]
-            civitai.CivitaiDownloader.download_model(model_id, cached_file)
+            download_model(model_id, cached_file)
         else:
             download_url_to_file(url, cached_file, progress=progress)
     return cached_file
